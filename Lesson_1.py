7cbfdc73664eb468e49804606c0af64907f2183d
Лев Мучник <lionrav19@gmail.com>
2022-10-06 00:47:23 +0300

    ДЗ номер 1

---
+++ Lesson_1.py
@@ -0,0 +1,58 @@
+# 1
+
+a = 5
+b = 5
+c = 5
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+# 2
+
+a1 = [4]
+b1 = [4]
+
+print(type(a1))
+print(type(b1))
+
+print(a1)
+print(b1)
+
+print(id(a1))
+print(id(b1))
+
+# 3
+# Первые 3-и
+
+a = [5]
+b = [5]
+c = [5]
+print(a,b,c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(id(a),id(b),id(c))
+
+# Вторые 2-е
+
+a1 = 4
+b1 = 4
+
+print(a1,b1)
+
+print(type(a1),type(b1))
+
+print(id(a1),id(b1))
+
+# ВОПРОС: Ничего если я какие-то команды к примеру тот же принт пишу несколько действий через запятую?
+# Можно ведь так ? Вроде все работает , но запятая красным подчекнута. Это и смущает))))
