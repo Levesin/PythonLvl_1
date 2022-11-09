e1a688e2d97fe6e962eb70abe43ccd9023efcc35
Лев Мучник <lionrav19@gmail.com>
2022-11-09 20:26:45 +0200

    сsv

---
+++ Home_17.py
@@ -0,0 +1,43 @@
+# Прочитать сохранённый json-файл из задания №16 и записать данные
+# на диск в csv-файл, первой строкой которого озаглавив каждый
+# столбец и добавив новый столбец “телефон”.
+
+
+import json
+import csv
+from pprint import pprint
+import pandas as pd
+
+
+
+with open("umba_tumba.json", "r", encoding="utf-8") as f:
+    chitaem = json.load(f)
+    pprint(chitaem)
+
+
+with open("umba_tumba.csv", "w", encoding="utf-8") as f:
+    file_csv = csv.writer(f)
+    file_csv.writerow(['Имя','Возраст','Телефон'])
+    file_csv.writerow(chitaem['111111'])
+    file_csv.writerow(chitaem['222222'])
+    file_csv.writerow(chitaem['333333'])
+    file_csv.writerow(chitaem['444444'])
+    file_csv.writerow(chitaem['555555'])
+    file_csv.writerow(chitaem['666666'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open("umba_tumba.csv", "w", newline='') as csv_file:
+#     writer = csv.writer(csv_file, delimiter=',')
+#     for line in chitaem:
+#         writer.writerow(line)
