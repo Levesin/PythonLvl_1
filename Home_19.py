42523c4efc847840ba274bc22aa6a82b129294a5
Лев Мучник <lionrav19@gmail.com>
2022-11-12 13:57:36 +0200

    ООП1

---
+++ Home_19.py
@@ -0,0 +1,36 @@
+# Создать родительский класс auto, у которого есть атрибуты:
+# brand, age, cоlor, mark и weight.
+# А так же методы: move, birthday и stop.
+# Методы move и stop выводят сообщение на экран «move» и «stop»,
+# а birthday увеличивает атрибут age на 1.
+# Атрибуты brand, age и mark являются обязательными при объявлении объекта.
+
+class auto:
+    brand = "Toyota"
+    age = 6
+    color = "Blue"
+    makr = "Сrossover"
+    weight = 1800
+
+    def move(self):
+        print("move")
+
+    def stop(self):
+        print("stop")
+
+    def birthday(self):
+        self.age+=1
+
+
+
+
+call_auto = auto()
+
+call_auto.move()
+call_auto.stop()
+call_auto.birthday()
+print(call_auto.age)
+call_auto.birthday()
+print(call_auto.age)
+call_auto.birthday()
+print(call_auto.age)
