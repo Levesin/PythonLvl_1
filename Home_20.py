821b0d712e72319cc5319092907ffcb69544308f
Лев Мучник <lionrav19@gmail.com>
2022-11-14 15:15:33 +0200

    ОПП 2

--- Home_20.py
+++ Home_20.py
@@ -10,3 +10,74 @@
 # обязательного атрибута max_load.
 # Создать по 2 объекта для каждого из классов truck и car,
 # проверить все их методы и атрибуты.
+import time
+
+class Auto:
+    brand = "Toyota"
+    age = 6
+    makr = "Сrossover"
+
+    def __init__(self, age, brand, mark, color="Blue", weight=1800):
+        self.age = age
+        self.brand = brand
+        self.makr = mark
+        self.color = "Blue"
+        self.weight = "1800"
+
+
+    def move(self):
+        print("move")
+
+    def stop(self):
+        print("stop")
+#_________________________________________________________________________________
+class Truck(Auto):
+    def __init__(self, max_load):
+        super().__init__(max_load, brand="Volvo", mark="24t")
+        self.max_load = max_load
+
+    def move(self):
+        print("attention")
+        super().move()
+
+
+    def load(self):
+        time.sleep(1)
+        print("load...")
+        time.sleep(1)
+#_________________________________________________________________________________
+class Car(Auto):
+    def __init__(self, max_speed, max_load="Twenty minutes"):
+        super().__init__(age=4, brand="Honda", mark="Hatchback",)
+        self.max_speed = max_speed
+        self.max_load = max_load
+        print(max_load)
+
+    def move(self):
+        super().move()
+        print("max speed is max_speed")
+
+
+call_auto = Auto(8, "Mazda", "coupe")
+call_auto.move()
+call_auto.stop()
+
+print("-"*50)
+
+call_Truck = Truck("Twenty minutes")
+call_two_Truck = Truck("Thirty minutes")
+
+call_Truck.move()
+call_two_Truck.move()
+
+call_Truck.load()
+call_two_Truck.load()
+
+print("-"*50)
+
+call_Car = Car("140")
+call_two_car = Car("180")
+
+call_Car.move()
+call_two_car.move()
+
