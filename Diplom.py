4992c858526ca14c191c4de5ed28f03b9f19a7fc
Лев Мучник <lionrav19@gmail.com>
2022-11-25 10:44:40 +0200

    Диплом

---
+++ Diplom.py
@@ -0,0 +1,32 @@
+#Дипломная работа
+class Person_data():
+
+    def load_person(self):
+        pass
+
+    def years_preson(self):
+        pass
+
+    def exit_data(self):
+        pass
+
+print("ГЛАВНОЕ МЕНЮ:\n")
+print("1.Загрузка в базу данных\n")
+print("2.Расчет кол-во полных лет\n")
+print("3.Поиск по базе\n")
+print("Для перехода по МЕНЮ введите номер операции\n")
+print("Хотите выйти? (Y/Д)\n")
+
+while True:
+    menu = input("Ввод: ")
+    if menu == 1:
+
+
+
+
+
+
+
+
+
+
